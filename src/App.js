64a9736cb954d4bba4d5542e3f13f115ff1f1279
Alin Pandichi <alin.pandichi@mozaicworks.com>
2018-06-08 06:36:34 +0300

    Start from scratch

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
 import React, {Component} from 'react';
-import Button from 'react-toolbox/lib/button/Button';
-import Sessions from "./components/Sessions";
 import {connect} from "react-redux";
-import NewSessionDialog from "./components/NewSessionDialog";
-import {showDialog} from "./redux/dialogReducer";
-import HiddenProgressBar from "./components/HiddenProgressBar";
-import SessionFormWithFields from "./components/SessionFormWithFields";
-import SessionFormWithInputs from "./components/SessionFormWithInputs";
-import SessionFormWithAutosuggestAutocomplete from "./components/SessionFormWithAutosuggestAutocomplete";
 
 class App extends Component {
-
-    showDialogToAddSlot = () => {
-        this.props.dispatch(showDialog(true))
-    };
 
     render() {
         return (
             <div className="App">
-                <Sessions sessions={this.props.sessions}/>
-                <Button icon='add' label='Add slot' raised primary onClick={this.showDialogToAddSlot} />
-                <NewSessionDialog>
-                    <HiddenProgressBar />
-                    <SessionFormWithAutosuggestAutocomplete />
-                </NewSessionDialog>
+                Blank app
             </div>
         );
     }
