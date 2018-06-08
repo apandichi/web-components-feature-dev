6abee12ed432396624944a8ce442e054691a96fd
Alin Pandichi <alin.pandichi@mozaicworks.com>
2018-06-08 12:42:14 +0300

    Added button to add slot, doesn't do anything for now

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {connect} from "react-redux";
 import {Card, CardMedia, CardTitle, CardText, CardActions} from 'react-toolbox/lib/card';
 import SessionCard from "./components/SessionCard";
 import Sessions from "./components/Sessions";
+import Button from 'react-toolbox/lib/button/Button';
 
 class App extends Component {
 
@@ -11,6 +12,7 @@ class App extends Component {
         return (
             <div className="App">
                 <Sessions sessions={sessions} />
+                <Button icon='add' label='Add session' raised primary />
             </div>
         );
     }
