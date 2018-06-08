a1da5fddb5eeed53012c1ab93c2493e535f3614e
Alin Pandichi <alin.pandichi@mozaicworks.com>
2018-06-08 12:32:33 +0300

    Add example card to the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, {Component} from 'react';
 import {connect} from "react-redux";
+import { Card, CardMedia, CardTitle, CardText, CardActions } from 'react-toolbox/lib/card';
 
 class App extends Component {
 
     render() {
         return (
             <div className="App">
-                Blank app
+                <Card style={{width: '350px'}}>
+                    <CardTitle
+                        avatar="https://placeimg.com/80/80/animals"
+                        title="Avatar style title"
+                        subtitle="Subtitle here"
+                    />
+                </Card>
             </div>
         );
     }
