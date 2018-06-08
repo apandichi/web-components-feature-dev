5f1d5b47c5e960ad8dcf98760eda03a25725d760
Alin Pandichi <alin.pandichi@mozaicworks.com>
2018-06-08 12:45:13 +0300

    Added an example dialog shown when clicking on the button

--- src/App.js
+++ src/App.js
@@ -4,15 +4,35 @@ import {Card, CardMedia, CardTitle, CardText, CardActions} from 'react-toolbox/l
 import SessionCard from "./components/SessionCard";
 import Sessions from "./components/Sessions";
 import Button from 'react-toolbox/lib/button/Button';
+import Dialog from 'react-toolbox/lib/dialog/Dialog';
 
 class App extends Component {
+
+    state = {
+        active: false
+    };
+
+    handleToggle = () => {
+        this.setState({active: !this.state.active});
+    };
 
     render() {
         const sessions = this.props.sessions;
         return (
             <div className="App">
                 <Sessions sessions={sessions} />
-                <Button icon='add' label='Add session' raised primary />
+                <Button icon='add' label='Add session' raised primary
+                    onClick={this.handleToggle}
+                />
+
+                <Dialog
+                    active={this.state.active}
+                    onEscKeyDown={this.handleToggle}
+                    onOverlayClick={this.handleToggle}
+                    title='My awesome dialog'
+                >
+                    <p>Here you can add arbitrary content. Components like Pickers are using dialogs now.</p>
+                </Dialog>
             </div>
         );
     }
