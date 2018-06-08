95824d97408829be33e6e91adaac0fd860e27213
Alin Pandichi <alin.pandichi@mozaicworks.com>
2018-06-08 12:39:32 +0300

    Extracted a session card component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from "react-redux";
 import {Card, CardMedia, CardTitle, CardText, CardActions} from 'react-toolbox/lib/card';
+import SessionCard from "./components/SessionCard";
 
 class App extends Component {
 
@@ -9,13 +10,7 @@ class App extends Component {
         return (
             <div className="App">
                 {sessions.map((session) => {
-                    return <Card style={{width: '350px'}}>
-                        <CardTitle
-                            avatar={session.avatar}
-                            title={session.name}
-                            subtitle={session.talk}
-                        />
-                    </Card>
+                    return <SessionCard session={session}/>
                 })}
             </div>
         );
